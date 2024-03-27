d6cfdf07eb3d9cb6158ce6a2ff95cfb7c320415a
Grudge69 <shubham.shubham.rawat090@gmail.com>
2024-03-27 22:11:44 +0530

    first commit

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Loader2, Search } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useRef, useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
+
+/* 
+    GOALS: 
+    1. Needs to be intuitive(hitting ESC while typing will loose focus from input element)
+    2. Functional(store the search results in URL params to give results on a reload)
+*/
+const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isSearching, startTransition] = useTransition();
+  const router = useRouter();
+  const [query, setQuery] = useState<string>("");
+
+  const search = () => {
+    startTransition(() => {
+      router.push(`/search?query=${query}`);
+    });
+  };
+
+  return (
+    <div className="relative w-full h-14 flex flex-col">
+      <div className="relative h-14 z-10 rounded-md">
+        <Input
+          disabled={isSearching}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              search();
+            }
+
+            // hitting ESC while typing will loose focus from input element
+            if (e.key === "Escape") {
+              inputRef?.current?.blur();
+            }
+          }}
+          ref={inputRef}
+          className="absolute inset-0 h-full"
+        />
+
+        <Button
+          disabled={isSearching}
+          className="absolute right-0 inset-y-0 h-full rounded-l-none"
+          size="sm"
+          onClick={search}
+        >
+          {isSearching ? (
+            <Loader2 className="h-6 w-6 animate-spin" />
+          ) : (
+            <Search className="h-6 w-6" />
+          )}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
